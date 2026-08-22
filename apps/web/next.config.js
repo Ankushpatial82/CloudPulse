6d974957d28ca089b30c74fa565dea49e5d952fe
Ankush patial <ankushpatial@Ankushs-MacBook-Air.local>
2026-08-22 22:31:50 +0530

    Fix Vercel output directory

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  output: "export",
   trailingSlash: true,
+
   images: {
     unoptimized: true,
   },
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
